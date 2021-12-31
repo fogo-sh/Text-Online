1c25eac0ac014d54138f4a4d678e0f0fbd25d7c0
Jack Arthur Harrhy <me@jackharrhy.com>
2021-12-31 12:03:51 -0330

    cleanup types, display internal client-side only messages when connecting to the backend

--- client/src/chat.ts
+++ client/src/chat.ts
@@ -1,22 +1,27 @@
 import { log } from "./log";
-import { IncomingMessage } from "./types";
+import { AnyMessage, IncomingMessage, MESSAGE_TYPES } from "./types";
 import { client, sendMessage } from "./websockets";
 
-const renderMessage = ({ message }: IncomingMessage) => {
+const renderMessage = (
+  { message }: AnyMessage,
+  customClasses: string[] = []
+) => {
   const messageElement = document.createElement("div");
-  messageElement.className = "message";
-  messageElement.innerText = `Mitch: ${message}`;
+  messageElement.classList.add(...["message", ...customClasses]);
+  messageElement.innerText = message;
   return messageElement;
 };
 
-const elementForMessage = (message: IncomingMessage) => {
+const elementForMessage = (message: AnyMessage) => {
   switch (message.type) {
-    case "ack": // a message you've sent yourself coming back to you
+    case MESSAGE_TYPES.ACK: // a message you've sent yourself coming back to you
       return renderMessage(message);
-    case "msg": // generic messages from other users / entities
+    case MESSAGE_TYPES.MSG: // generic messages from other users / entities
       return renderMessage(message);
+    case MESSAGE_TYPES.INT: // an internal message generated clientside
+      return renderMessage(message, ["message-internal"]);
     default:
-      throw new Error(`Unknown message type`);
+      throw new Error(`Unknown message type: ${message.type}`);
   }
 };
 
@@ -24,11 +29,24 @@ export const chatOutput = () => {
   const chat = document.createElement("div");
   chat.className = "chat";
 
+  const displayMessage = (message: AnyMessage) => {
+    const element = elementForMessage(message);
+    chat.prepend(element);
+  };
+
+  const displayInternalMessage = (message: string) =>
+    displayMessage({ type: MESSAGE_TYPES.INT, message });
+
+  displayInternalMessage("Connecting to Text Online...");
+
+  client.onopen = () => displayInternalMessage("Connected to Text Online!");
+  client.onclose = () =>
+    displayInternalMessage("Disconnected from Text Online");
+
   client.onmessage = (event: MessageEvent) => {
     const incomingMessage = JSON.parse(event.data) as IncomingMessage;
     log("Incoming message", incomingMessage);
-    const element = elementForMessage(incomingMessage);
-    chat.prepend(element);
+    displayMessage(incomingMessage);
   };
 
   return chat;
--- client/src/types.ts
+++ client/src/types.ts
@@ -1,18 +1,22 @@
-export type AckMessage = {
-  type: "ack";
+export const MESSAGE_TYPES = {
+  INT: "int",
+  ACK: "ack",
+  MSG: "msg",
+  MESSAGE: "message",
+};
+
+export type Message<Type> = {
+  type: Type;
   message: string;
 };
 
-export type ChatMessage = {
-  type: "msg";
-  message: string;
-};
+export type AnyMessage = Message<string>;
 
+export type InternalMessage = Message<"int">;
+
+export type AckMessage = Message<"ack">;
+export type ChatMessage = Message<"msg">;
 export type IncomingMessage = AckMessage | ChatMessage;
 
-export type TypedMessage = {
-  type: "message";
-  message: string;
-};
-
+export type TypedMessage = Message<"message">;
 export type OutgoingMessage = TypedMessage;
